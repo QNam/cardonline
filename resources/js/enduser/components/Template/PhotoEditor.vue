5751e2dc4e13c4bfa2f53ecb7912093ed32f751d
QNam <namdoanquoc.1998@gmail.com>
2021-06-12 05:27:59 +0700

    update

--- resources/js/enduser/components/Template/PhotoEditor.vue
+++ resources/js/enduser/components/Template/PhotoEditor.vue
@@ -5,6 +5,7 @@
             adjustStencil: false
         }" -->
     <cropper
+        ref="cropper"
         :src="img"
         :stencil-props="{
             handlers: {},
@@ -15,12 +16,19 @@
         image-restriction="stencil"
         @change="change"
     ></cropper>
+    <div class="action-croppers">
+        <a @click.stop="flip(true,false)" v-html="icons.flip_horizontally"></a>
+        <a @click.stop="flip(false,true)" v-html="icons.flip_vertically"></a>
+        <a @click.stop="rotate(90)" v-html="icons.rotate_right"></a>
+        <a @click.stop="rotate(-90)" v-html="icons.rotate_left"></a>
+    </div>
 </div>
 </template>
 
 <script>
 import { Cropper } from 'vue-advanced-cropper'
 import 'vue-advanced-cropper/dist/style.css';
+import icons from './icons'
 
 export default {
     props: {
@@ -43,6 +51,7 @@ export default {
     },
     data: function() {
         return {
+            icons: icons
         }
     },
     methods: {
@@ -57,11 +66,34 @@ export default {
             this.$emit('change', canvas.toDataURL())
             // console.log(coordinates, canvas)
             // console.log(canvas.toDataURL())
-        }
+        },
+        flip(x,y) {
+			this.$refs.cropper.flip(x,y);
+		},
+		rotate(angle) {
+			this.$refs.cropper.rotate(angle);
+		},
     },
 }
 </script>
 
-<style>
+<style scoped>
+.action-croppers {
+    display: flex;
+    margin-top: 10px;
+    justify-content: center;
+    /* flex-direction: column; */
+}
 
+.action-croppers a {
+    background: rgba(0,0,0,.4);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 42px;
+    width: 42px;
+    margin-right: 10px;
+    cursor: pointer;
+    transition: background .5s;
+}
 </style>
---
+++ resources/js/enduser/components/Template/icons.js
@@ -0,0 +1,78 @@
+export default {
+    'rotate_right': `
+        <?xml version="1.0" encoding="utf-8"?>
+        <!-- Generator: Adobe Illustrator 18.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            width="16px" height="16px" viewBox="0 0 16 16" enable-background="new 0 0 16 16" xml:space="preserve">
+        <g enable-background="new    ">
+            <path fill="#FFFFFF" d="M1.5,8c0-3.7,3-6.5,6.5-6.5c1.7,0,3.3,0.7,4.5,1.8l1.1-1.1c0.1-0.1,0.4-0.1,0.6-0.1
+                c0.1,0.1,0.3,0.3,0.3,0.6v3.8c0,0.3-0.3,0.6-0.6,0.6h-3.8C10,6.9,9.8,6.7,9.7,6.6C9.6,6.4,9.7,6.2,9.8,6L11,4.9
+                C10.1,4,9.1,3.6,8,3.6c-2.4,0-4.4,2-4.4,4.4s2,4.4,4.4,4.4c1.4,0,2.5-0.6,3.4-1.7l0.1-0.1c0.1,0,0.1,0,0.3,0.1l1.1,1.1
+                c0.1,0.1,0.1,0.3,0,0.4c-1.1,1.4-3,2.3-4.9,2.3C4.3,14.5,1.5,11.7,1.5,8z"/>
+        </g>
+        </svg>
+        `,
+    'rotate_left': `
+        <?xml version="1.0" encoding="utf-8"?>
+        <!-- Generator: Adobe Illustrator 18.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            width="16px" height="16px" viewBox="0 0 16 16" enable-background="new 0 0 16 16" xml:space="preserve">
+        <g enable-background="new    ">
+            <path fill="#FFFFFF" d="M8,14.5c-2,0-3.8-0.8-5.1-2.4c-0.1-0.1-0.1-0.3,0-0.4l1.1-1.1c0,0,0.1-0.1,0.3-0.1c0.1,0,0.1,0,0.1,0.1
+                c1,1.3,2.1,1.8,3.5,1.8c2.4,0,4.4-2,4.4-4.4s-2-4.4-4.4-4.4c-1.1,0-2.1,0.4-3,1.1l1.1,1.1c0.1,0.3,0.3,0.4,0.1,0.7
+                C6.2,6.7,6.1,6.9,5.9,6.9H2c-0.3,0-0.6-0.3-0.6-0.6V2.5c0-0.3,0.1-0.4,0.3-0.6c0.1-0.1,0.4,0,0.6,0.1l1.1,1.1
+                c1.3-1.1,2.8-1.8,4.5-1.8c3.7,0,6.5,3,6.5,6.5S11.7,14.5,8,14.5z"/>
+        </g>
+        </svg>`,
+    'flip_horizontally': `
+        <?xml version="1.0" encoding="utf-8"?>
+        <!-- Generator: Adobe Illustrator 18.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            width="16px" height="16px" viewBox="0 0 16 16" enable-background="new 0 0 16 16" xml:space="preserve">
+        <line fill="#FFFFFF" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" x1="8" y1="1" x2="8" y2="14.9"/>
+        <g>
+            <g>
+                
+                    <line fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" x1="11.5" y1="12.8" x2="12" y2="12.8"/>
+                <polyline fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" points="14.2,12.8 
+                    14.7,12.8 14.7,12.3 		"/>
+                
+                    <line fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.8091,2.4273" x1="14.7" y1="9.9" x2="14.7" y2="4.6"/>
+                <polyline fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" points="14.7,3.4 
+                    14.7,2.9 14.2,2.9 		"/>
+                
+                    <line fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" x1="12" y1="2.9" x2="11.5" y2="2.9"/>
+            </g>
+        </g>
+        <polyline fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linejoin="round" stroke-miterlimit="10" points="4.5,2.9 
+            1.3,2.9 1.3,12.8 4.5,12.8 "/>
+        </svg>`,
+
+    'flip_vertically': `
+        <?xml version="1.0" encoding="utf-8"?>
+        <!-- Generator: Adobe Illustrator 18.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            width="16px" height="16px" viewBox="0 0 16 16" enable-background="new 0 0 16 16" xml:space="preserve">
+        <line fill="#FFFFFF" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" x1="15" y1="8" x2="1" y2="8"/>
+        <g>
+            <g>
+                
+                    <line fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" x1="3.2" y1="11.5" x2="3.2" y2="12"/>
+                <polyline fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" points="3.2,14.2 
+                    3.2,14.7 3.7,14.7 		"/>
+                
+                    <line fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.8091,2.4273" x1="6.1" y1="14.7" x2="11.4" y2="14.7"/>
+                <polyline fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" points="12.6,14.7 
+                    13.1,14.7 13.1,14.2 		"/>
+                
+                    <line fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" x1="13.1" y1="12" x2="13.1" y2="11.5"/>
+            </g>
+        </g>
+        <polyline fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linejoin="round" stroke-miterlimit="10" points="13.1,4.4 
+            13.1,1.3 3.2,1.3 3.2,4.4 "/>
+        </svg>`
+}
