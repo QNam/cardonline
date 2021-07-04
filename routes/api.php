d5f8ca223f7d354a612e6af63b5d22d030caa775
QNam <namdoanquoc.1998@gmail.com>
2021-07-04 23:19:12 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -23,22 +23,25 @@ Route::post('/card/register', 'App\Http\Controllers\AuthController@register');
 Route::post('/card/login', 'App\Http\Controllers\AuthController@Login');
 Route::post('/card/exists', 'App\Http\Controllers\CardController@cardIsExists');
 Route::get('/card/getList', 'App\Http\Controllers\CardController@getListCard');
-Route::post('/card', 'App\Http\Controllers\CardController@storeCard');
-Route::get('/card/getById', 'App\Http\Controllers\CardController@getById');
-Route::post('/card/remove', 'App\Http\Controllers\CardController@removeCard');
 
-Route::post('/media/uploadImage', 'App\Http\Controllers\MediaController@uploadImage');
-Route::post('/media/uploadImageBase64', 'App\Http\Controllers\MediaController@uploadImageBase64');
+Route::group(['middleware' => ['web', 'admin.checkLogin']], function () {
+    Route::post('/card/remove', 'App\Http\Controllers\CardController@removeCard');
+    Route::post('/card/updateTickCard', 'App\Http\Controllers\CardController@updateTickCard');
+    Route::post('/card/storeOneCard', 'App\Http\Controllers\CardController@storeOneCard');
+    Route::post('/card/genCard', 'App\Http\Controllers\CardController@genCard');
+});
 
-Route::post('/card/saveAvatar', 'App\Http\Controllers\CardController@saveAvatar');
-Route::post('/card/saveBackground', 'App\Http\Controllers\CardController@saveBackground');
-Route::post('/card/saveBackgroundBase64', 'App\Http\Controllers\CardController@saveBackgroundBase64');
-Route::post('/card/removeLink', 'App\Http\Controllers\CardController@removeCardLink');
-Route::post('/card/genCard', 'App\Http\Controllers\CardController@genCard');
+Route::group(['middleware' => ['web', 'user.checkLogin']], function () {
+    Route::get('/card/getById', 'App\Http\Controllers\CardController@getById');
+    Route::post('/card', 'App\Http\Controllers\CardController@storeCard');
+    Route::post('/media/uploadImage', 'App\Http\Controllers\MediaController@uploadImage');
+    Route::post('/media/uploadImageBase64', 'App\Http\Controllers\MediaController@uploadImageBase64');
+    Route::post('/card/saveAvatar', 'App\Http\Controllers\CardController@saveAvatar');
+    Route::post('/card/saveBackground', 'App\Http\Controllers\CardController@saveBackground');
+    Route::post('/card/saveBackgroundBase64', 'App\Http\Controllers\CardController@saveBackgroundBase64');
+    Route::post('/card/removeLink', 'App\Http\Controllers\CardController@removeCardLink');
+});
+
 Route::post('/card/checkConfirmCode', 'App\Http\Controllers\CardController@checkConfirmCode');
 Route::post('/card/checkAccountToForgetPassword', 'App\Http\Controllers\CardController@checkAccountToForgetPassword');
 Route::post('/card/forgetPassword', 'App\Http\Controllers\CardController@forgetPassword');
-
-Route::post('/card/updateTickCard', 'App\Http\Controllers\CardController@updateTickCard');
-
-Route::post('/card/storeOneCard', 'App\Http\Controllers\CardController@storeOneCard');
