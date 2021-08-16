21433c921f8ee0e761de23f2b6c515cdfecdf501
QNam <namdoanquoc.1998@gmail.com>
2021-08-16 19:29:01 +0700

    update

--- config/sites.php
+++ config/sites.php
@@ -2,6 +2,7 @@
 
 return [
     'SITES_LOGO' => [
+        'luxury50.com' => asset('imgs/LOGO_NAM_KAY.png'),
         'namkay.com' => asset('imgs/LOGO_NAM_KAY.png'),
         'luxury40.com' => asset('imgs/LUXURY40_BLACK.png'),
         'fuki40.com' => asset('static/imgs/logo.png') 
