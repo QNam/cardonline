1db1f93fa309bb04994852702a1075cad1cac7bd
QNam <namdoanquoc.1998@gmail.com>
2021-08-23 21:30:59 +0700

    update

--- config/sites.php
+++ config/sites.php
@@ -6,7 +6,7 @@ return [
         'namkay.com' => asset('imgs/LOGO_NAM_KAY.png'),
         'luxury40.com' => asset('imgs/LUXURY40_BLACK.png'),
         'fuki40.com' => asset('static/imgs/logo.png'), 
-        'ktv50xs.com' =>  asset('static/imgs/ktv50xs.png')
+        'ktv50xs.com' =>  asset('imgs/ktv50xs.png')
     ],
 
     'SITES_NOT_HOME' => [
