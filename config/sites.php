a8d129a94255686e61170b6cde4c60a163f0cc42
QNam <namdoanquoc.1998@gmail.com>
2021-08-23 21:23:17 +0700

    update

--- config/sites.php
+++ config/sites.php
@@ -5,13 +5,15 @@ return [
         'luxury50.com' => asset('imgs/LOGO_NAM_KAY.png'),
         'namkay.com' => asset('imgs/LOGO_NAM_KAY.png'),
         'luxury40.com' => asset('imgs/LUXURY40_BLACK.png'),
-        'fuki40.com' => asset('static/imgs/logo.png') 
+        'fuki40.com' => asset('static/imgs/logo.png'), 
+        'ktv50xs.com' =>  asset('static/imgs/ktv50xs.png')
     ],
 
     'SITES_NOT_HOME' => [
         'namkay.com',
         'luxury40.com',
         'luxury50.com',
+        'ktv50xs.com'
         // 'cardonline.local',
     ]
 ];
